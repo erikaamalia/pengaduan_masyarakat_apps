0ef519f675c4e5d2b9b26fb8a9737b5df632c363
Hosnol Arifin <hosnolarifin220@gmail.com>
2021-12-11 11:07:46 +0700

    validasi nomer nik

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,17 +32,25 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
+        $nik = $request->nik;
+        $arnik = str_split($nik);
+        $kodedaerah = '351204';
+        $validasikodedaerah = $arnik[0].$arnik[1].$arnik[2].$arnik[3].$arnik[4].$arnik[5];
+
+        if($validasikodedaerah != $kodedaerah){
+            return back()->withInput()->withErrors(['NIK Anda tidak valid!!']);
+        }
+
         $request->validate([
             'nik' => 'required|string|digits:16|unique:users',
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'phone' => 'required|string|max:12',
             'password' => 'required|string|confirmed|min:8',
-
         ]);
 
         Auth::login($user = User::create([
-            'nik' => $request->nik,
+            'nik' => $nik,
             'name' => $request->name,
             'email' => $request->email,
             'phone' => $request->phone,
